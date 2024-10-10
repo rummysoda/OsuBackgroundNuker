c914d0304e35f553a72a031a6c99d77eef131042
AmineLeCepe <lionannaba@gmail.com>
2024-10-10 20:43:09 +0100

    Add support for custom directories

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 import os
 import time
+import typer
 
 CURRENT_USER = os.getlogin()
 PATH_TO_GAME = f"C:\\Users\\{CURRENT_USER}\\AppData\\Local\\osu!\\Songs" # Assumes that the osu folder is in the default installation location
-SONG_FOLDER_FILES = os.listdir(PATH_TO_GAME)
 
-beatmaps = []
+def nuke(path): # Function that deletes all the beatmap backgrounds in the specified song folder
+    beatmaps = []
 
-def delete_all_beatmaps():
-    for file in SONG_FOLDER_FILES:
+    os.chdir(path) # Changes directory to the specified path (or default path if no CLI argument has been passed)
+
+    song_folder_files = os.listdir(path)
+    for file in song_folder_files:
         # Pushes all the song folders to the "beatmaps" list
         if os.path.isdir(file):
             beatmaps.append(file)
 
     for beatmap in beatmaps:
-        os.chdir(PATH_TO_GAME + f"\\{beatmap}")
+        os.chdir(path + f"\\{beatmap}")
         file_list = os.listdir()
         for file in file_list:
             # Checks for the file extensions and deletes the files accordingly
@@ -25,14 +28,20 @@ def delete_all_beatmaps():
 
     print("Deleted all map backgrounds!")
 
-confirm = input("Enter 'CONFIRM' to delete all of your beatmap backgrounds: ")
+def main(path: str = PATH_TO_GAME): # Main function
+    print(f"Target folder: {path}\n")
+    confirm = input("Enter 'CONFIRM' to delete all of your beatmap backgrounds: ")
 
-if confirm == "CONFIRM":
-    delete_all_beatmaps()
-else:
-    print("Exiting...")
-    time.sleep(2)
+    if confirm == "CONFIRM":
+        nuke(path)
+
+    else:
+        print("Exiting...")
+        time.sleep(2)
+
+if __name__ == "__main__":
+    typer.run(main)
 
 
-os.chdir(PATH_TO_GAME)
+
 
